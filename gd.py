2697e04374fd47bd5499d682b9ae0ea9c73e0d4c
Shunji Zhan <shunhahaha@live.com>
2017-11-17 15:39:59 -0800

    almost done

--- gd.py
+++ gd.py
@@ -1,68 +1,104 @@
 import numpy as np
 from random import randint
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from sklearn.preprocessing import normalize
 
 
-# gradient of f(X) = (X*Beta - y) ^ 2 / N
-def gradient(X, Y, beta):
-    g = np.zeros(X.shape[1])
-    for i in range(X.shape[0]):
-        xi_T = X[i, :]
-        xi = np.transpose(xi_T)
-        yi = Y[i]
-        g += xi * (xi_T.dot(beta) - yi) * 2 / X.shape[0]
-    return g
+def n(v):
+    return normalize(v[:, np.newaxis], axis=0).ravel()
 
 
-# estimator of gradient of f(X) = (X*Beta - y) ^ 2 / N
-def stochastic_gradient(X, Y, beta):
-    i = randint(0, X.shape[0])
-    xi_T = X[i, :]
-    xi = np.transpose(xi_T)
-    yi = Y[i]
-    return xi * (xi_T.dot(beta) - yi) * 2 / X.shape[0]
-
-
-def sqr_error(X, Y, beta):
-    temp = X.dot(beta) - Y
-    return temp.dot(np.transpose(temp)) / X.shape[0]
-
-
-# Linear Regression implementation
-class LinearRegression:
+class GradientDescent:
     def __init__(self):
-        self.step_size = 0.025
         self.n, self.d = 6000, 100
-
-        self.X = np.random.rand(self.n, self.d)
+        self.X = np.random.normal(size=(self.n, self.d))
         self.beta_real = np.random.rand(self.d)
         self.Y = self.X.dot(self.beta_real)
 
-        self.beta_rand = np.random.rand(self.d)   # initial beta for GD
-        self.GD()
-        self.SGD()
+        self.main()
 
-    def GD(self):
-        beta_GD = self.beta_rand
-        errors_GD = []
+    def main(self):
+        beta_rand = np.random.rand(self.d)  # initial beta for GD
+        errors_GD = self.GD(np.copy(beta_rand))
+        errors_NAGD = self.NAGD(np.copy(beta_rand))
+        errors_SGD = self.SGD(np.copy(beta_rand))
+
+        print errors_GD[:5]
+        print errors_NAGD[:5]
+        print errors_SGD[:5]
+        # set y axis to display integer
+        # fig, ax = plt.subplots()
+        # ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+        plt.xlabel('iterations')
+        plt.ylabel('f(x)')
+        X = np.arange(50)
+        plt.plot(X, errors_GD, 'r-', X, errors_NAGD, 'b-')
+        plt.show()
+        plt.plot(errors_SGD)
+        plt.show()
+
+    def GD(self, beta):
+        step = 0.025
+        errors = []
         for i in range(50):
-            sqr_error_GD = sqr_error(self.X, self.Y, beta_GD)
-            errors_GD.append(sqr_error_GD)
-            beta_GD = beta_GD - self.step_size * gradient(self.X, self.Y, beta_GD)
-        print errors_GD
-        plt.plot(errors_GD, 'r')
-        plt.show()
+            sqr_error_ = self.sqr_error(beta)
+            errors.append(sqr_error_)
+            # update beta
+            beta -= step * self.gradient(beta)
+        return errors
 
-    def SGD(self):
-        beta_SGD = self.beta_rand
-        errors_SGD = []
-        for i in range(250):
-            sqr_error_SGD = sqr_error(self.X, self.Y, beta_SGD)
-            errors_SGD.append(sqr_error_SGD)
-            beta_SGD = beta_SGD - self.step_size * stochastic_gradient(self.X, self.Y, beta_SGD)
-        plt.plot(errors_SGD, 'b')
-        plt.show()
+    def NAGD(self, beta):
+        y, z = np.copy(beta), np.copy(beta)
+        step = 0.025
+        errors = []
+        for i in range(50):
+            sqr_error_ = self.sqr_error(beta)
+            errors.append(sqr_error_)
+            # update beta
+            g = self.gradient(beta)
+            a = 2.0 / (i + 3)
+            y = beta - step * g
+            z -= ((i + 1) / 2) * step * g
+            beta = (1 - a) * y + a * z
+        return errors
+
+    def SGD(self, beta):
+        step = 0.005
+        errors = []
+        for i in range(300):
+            sqr_error_ = self.sqr_error(beta)
+            errors.append(sqr_error_)
+            # update beta
+            beta = beta - step * self.stochastic_gradient(beta)
+        return errors
+
+    # gradient of f(X) = (X*Beta - y) ^ 2 / N
+    def gradient(self, beta):
+        X, Y = self.X, self.Y
+        g = np.zeros(X.shape[1])
+        for i in range(X.shape[0]):
+            xi = X[i, :]
+            yi = Y[i]
+            g += xi * (xi.dot(beta) - yi)
+        return g * 2 / X.shape[0]
+
+    # estimator of gradient of f(X) = (X*Beta - y) ^ 2 / N
+    def stochastic_gradient(self, beta):
+        X, Y = self.X, self.Y
+        i = randint(0, X.shape[0])
+        xi_T = X[i, :]
+        xi = np.transpose(xi_T)
+        yi = Y[i]
+        return xi * (xi_T.dot(beta) - yi) * 2
+
+    # squared error
+    def sqr_error(self, beta):
+        X, Y = self.X, self.Y
+        N = X.shape[0]
+        temp = X.dot(beta) - Y
+        return temp.dot(temp) / N
 
 
 if __name__ == '__main__':
-    lm = LinearRegression()
+    lm = GradientDescent()
